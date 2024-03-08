6210b01cbf1380bfc15949bfa12bb49f3b029524
marcoDmc <marcodamasceno07@gmail.com>
2024-03-08 09:14:35 -0300

    chore: imports and initial configurations

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,10 @@
+import Home from "@/app/page";
+import { Footer } from "../../../components/footer/Footer";
+import { VscSend } from "react-icons/vsc";
+
+export default function Login() {
+    return (<>
+
+    </>
+    )
+}
